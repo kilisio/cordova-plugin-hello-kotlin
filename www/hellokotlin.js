52666cad37cccae9d8971b7c128060b65818cf56
kilisio <pkilisio@gmail.com>
2020-12-17 20:46:36 +0300

    updated global entry module syntax

--- www/hellokotlin.js
+++ www/hellokotlin.js
@@ -1,7 +1,11 @@
-/*global cordova, module*/
-
-module.exports = {
-    hello: function (input, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "HelloKotlin", "hello", [input]);
+"use strict";
+var cordova_1 = require("cordova");
+var HelloKotlin = (function () {
+    function HelloKotlin() {
     }
-};
+    HelloKotlin.hello = function (input, successCallback, errorCallback) {
+        cordova_1.exec(successCallback, errorCallback, "HelloKotlin", "hello", [input]);
+    };
+}());
+
+module.exports = HelloKotlin;
